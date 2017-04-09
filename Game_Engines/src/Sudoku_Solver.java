792cca1ab8c61e3e0a41ca04c5fef13549d20edb
frankzhang99 <zhangf1999@gmail.com>
2017-04-09 11:14:00 -0700

    Wrote basic setup and a print method

--- Game_Engines/src/Sudoku_Solver.java
+++ Game_Engines/src/Sudoku_Solver.java
@@ -1,10 +1,32 @@
 
 public class Sudoku_Solver {
 
+	static int userInput[][] = new int[][]
+			   {{0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0, 0, 0, 0, 0}};
+			
+	static int grid[][] = new int[9][9];
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//Attempt for a sudoku solver
 		
-		
+		print(userInput);
+			
+	}
+	
+	public static void print(int[][] grid){
+		System.out.println();
+		for (int i = 0; i < grid.length; i++){
+			for (int j = 0; j < grid[i].length; j++){
+				System.out.print(grid[i][j]);
+			}
+			System.out.println();
+		}
 	}
 }
