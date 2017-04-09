8c78e277fbad9b182b847c6544608fc98b3a8ac8
Sai Bulusu <sai.bulusu@gmail.com>
2017-04-09 11:15:01 -0700

    Created basic setup for Tic Tac Toe

--- Game_Engines/src/Tic_Tac_Toe.java
+++ Game_Engines/src/Tic_Tac_Toe.java
@@ -1,6 +1,15 @@
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Tic_Tac_Toe {
 	public static void main(String[] args){
+//		3 by 3 version of Tic-Tac-Toe
+		boolean[][] check = new boolean[3][3];
+		boolean win = false;
+		
+		
+		ArrayList<HashMap<Integer, Integer>> moves = new ArrayList<>();
+		
 		
 	}
 }
