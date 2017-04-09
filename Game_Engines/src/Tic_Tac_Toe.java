844a8b0e2a8df6ffbf0bb3da3c66c3a0ddef32ce
Sai Bulusu <sai.bulusu@gmail.com>
2017-04-09 12:17:41 -0700

    Game Progression for Tic-Tac-Toe

--- Game_Engines/src/Tic_Tac_Toe.java
+++ Game_Engines/src/Tic_Tac_Toe.java
@@ -1,10 +1,12 @@
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.Stack;
 
 public class Tic_Tac_Toe {
 	public static void main(String[] args){
 //		3 by 3 version of Tic-Tac-Toe
-		boolean[][] check = new boolean[3][3];
+		boolean check[][] = new boolean[3][3];
+
 		boolean playerWin = false;
 		boolean computerWin = false;
 		boolean win = (playerWin || computerWin);
@@ -15,19 +17,35 @@ public class Tic_Tac_Toe {
 
 //		Must match winningCombinations to win
 //		Only 8 winning combinations
-		String[][] winningCombinations = {{"0", "01", "02"}, {"10", "10", "12"}, {"20", "21", "22"},
+		String[][] winningCombinations = {{"00", "01", "02"}, {"10", "10", "12"}, {"20", "21", "22"},
 				{"00", "11", "22"}, {"02", "11", "20"}, {"00", "10", "20"},  {"01", "11", "21"}, {"02", "12", "22"}};
 		
 		
-		System.out.println("Do you want to go first?");
+		System.out.println("Do you want to go first?(yes/no)");
 		
 //		Print color in the console to represent a checked position
 		Scanner scan = new Scanner(System.in);
 		
-		
-		while(!win){
-			String move = scan.nextLine();
-			
+		String answer = scan.nextLine();
+		if(answer.equals("yes")){
+//			Responsive
+			while(!win){
+				System.out.println("Your turn");
+				String move = scan.nextLine();
+				player.push(move);
+				
+				int move1 = Integer.valueOf(move.substring(0,1));
+				int move2 = Integer.valueOf(move.substring(1));
+//				check[move1] = ;
+//				Need to figure out how to fill in a 2-dim array of boolean values with a boolean value (?)
+				
+				
+				boolean taken = true;
+				while(taken){
+					int a = (int) Math.random() * 3;
+					int b = (int) Math.random() * 3;
+				}
+			}
 		}
 	}
 }
