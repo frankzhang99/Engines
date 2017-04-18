e80ef9aae345bf1172cde08635e2e5dab990cbfb
frankzhang99 <zhangf1999@gmail.com>
2017-04-17 21:00:35 -0700

    Added cases for no zeros

--- Game_Engines/src/LeetCodeThreeSum.java
+++ Game_Engines/src/LeetCodeThreeSum.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 public class LeetCodeThreeSum {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] test = {0, 0, 0, 1, -1, 2, 3, -4, -5, -3, -3, 1, 1};
+		int[] test = {0, 0, 0, 1, -1, 2, 3, -4, -5, -3, -3, 1, 1, 6};
 		print2(threeSum(test));
 		//System.out.println(Arrays.binarySearch(test, 5));
 	}
@@ -85,11 +85,68 @@ public class LeetCodeThreeSum {
 		
 		//for each int in pos
 		//find all possible combinations of 2 int in neg that adds up to that
+		int last_goal = -1;
+		for(int i = 0; i < pos.length; i++){
+			if(pos[i] != last_goal){
+				int goal = pos[i];
+				last_goal = goal;
+				
+				//check every 2 elements from neg
+				int first_checked = 1;
+				for(int j = 0; j < neg.length-1; j++){
+					int second_checked = 1;
+					if(neg[j] != first_checked){
+						first_checked = neg[j];
+						for(int k = j+1; k < neg.length; k++){
+							if(neg[k] != second_checked){
+								second_checked = neg[k];
+								if(first_checked + second_checked == -goal){
+									List<Integer> temp = new ArrayList<Integer>();
+									temp.add(goal);
+									temp.add(first_checked);
+									temp.add(second_checked);
+									answer.add(temp);
+								}
+							}
+						}
+					}
+				}
+			}
+			
+		}
 		
 		
 		//for each int in neg
 		//find pair of int in pos
-		
+		int last_goal_ = 1;
+		for(int i = 0; i < neg.length; i++){
+			if(neg[i] != last_goal_){
+				int goal = neg[i];
+				last_goal_ = goal;
+				
+				//check every 2 elements from pos
+				int first_checked = -1;
+				for(int j = 0; j < pos.length-1; j++){
+					int second_checked = -1;
+					if(pos[j] != first_checked){
+						first_checked = pos[j];
+						for(int k = j+1; k < pos.length; k++){
+							if(pos[k] != second_checked){
+								second_checked = pos[k];
+								if(first_checked + second_checked == -goal){
+									List<Integer> temp = new ArrayList<Integer>();
+									temp.add(goal);
+									temp.add(first_checked);
+									temp.add(second_checked);
+									answer.add(temp);
+								}
+							}
+						}
+					}
+				}
+			}
+			
+		}
 		
 		
 		return answer;
