8dfc360bb9e0c6b3274ff10887e87aba893cad34
frankzhang99 <zhangf1999@gmail.com>
2017-05-16 22:07:07 -0700

    Finished some menu setup

--- Game_Engines/src/tetris/TetrisMain.java
+++ Game_Engines/src/tetris/TetrisMain.java
@@ -1,16 +1,24 @@
+//https://www.youtube.com/watch?v=qPRWYyl41oQ&list=PLx2EVi2M-lqsJIFCbaDI-39Zh6NnIwJ1z
+
 package tetris;
 
 import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.image.BufferStrategy;
 
 import javax.swing.JFrame;;
 
 public class TetrisMain extends Canvas implements Runnable, KeyListener {
 
+	public static final int WIDTH = 400, HEIGHT = 540;
+	
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("Tetris");
-		frame.setSize(400, 540);
+		frame.setSize(WIDTH, HEIGHT);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLocationRelativeTo(null);
 		frame.setResizable(false);
@@ -18,11 +26,40 @@ public class TetrisMain extends Canvas implements Runnable, KeyListener {
 		TetrisMain tm = new TetrisMain();
 		frame.add(tm);
 		frame.setVisible(true);
-		
+		tm.start();
+	}
+	
+	public void start(){
+		Thread t = new Thread(this);
+		t.setPriority(Thread.MAX_PRIORITY);
+		t.start();
 	}
 
-	public void run() {
+	public void run(){
+		boolean running = true;
+		while(running){
+			update();
+			BufferStrategy buf = getBufferStrategy();
+			if(buf == null){
+				createBufferStrategy(3);
+				continue;
+			}
+			Graphics2D g = (Graphics2D) buf.getDrawGraphics();
+			render(g);
+			buf.show();
+		}
+	}
+	
+	public void update(){
 		
+	}
+	
+	public void render(Graphics2D g){
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 0, WIDTH, HEIGHT);
+		g.setColor(Color.WHITE);
+		g.setFont(new Font("Calibri", Font.PLAIN, 30));
+		g.drawString("Tetris", 170, 50);
 	}
 	
 	public void keyPressed(KeyEvent e){
