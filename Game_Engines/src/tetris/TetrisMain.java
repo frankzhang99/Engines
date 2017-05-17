a3d2605f409d05ba5c24f77aab3dc1d6099cac7c
frankzhang99 <zhangf1999@gmail.com>
2017-05-16 21:51:44 -0700

    Set config of basic JFrame

--- Game_Engines/src/tetris/TetrisMain.java
+++ Game_Engines/src/tetris/TetrisMain.java
@@ -1,10 +1,40 @@
 package tetris;
 
-public class TetrisMain {
+import java.awt.Canvas;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JFrame;;
+
+public class TetrisMain extends Canvas implements Runnable, KeyListener {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		JFrame frame = new JFrame("Tetris");
+		frame.setSize(400, 540);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocationRelativeTo(null);
+		frame.setResizable(false);
+		
+		TetrisMain tm = new TetrisMain();
+		frame.add(tm);
+		frame.setVisible(true);
+		
 	}
 
+	public void run() {
+		
+	}
+	
+	public void keyPressed(KeyEvent e){
+		
+	}
+	
+	public void keyTyped(KeyEvent e){
+		
+	}
+	
+	public void keyReleased(KeyEvent e){
+		
+	}
+	
 }
